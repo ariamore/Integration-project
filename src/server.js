22e9b8776f36c3bc5c1132178bc088988e4932b6
Mattias E. Mignone <memignone@gmail.com>
2019-02-05 12:28:45 +0100

    Added DB interaction to the app

--- src/server.js
+++ src/server.js
@@ -1,16 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 // Constants
+const DB_STATUS = ['disconnected', 'connected', 'connecting', 'disconnecting']
 const HOST = '0.0.0.0';
 const PORT = 9000;
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>\n');
+// DB connection
+mongoose.connect(process.env.DB_URL);
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose is now connected to ${process.env.DB_URL}`)
+});
+mongoose.connection.on('disconnected', () => {
+  console.log(`Mongoose is now disconnected.`)
+});
+mongoose.connection.on('error', () => {
+  console.log(`Mongoose encountered an error on the connection.`)
 });
 
-// app.listen(PORT, HOST);
+// App initialization
+const app = express();
+app.get('/', (req, res) => {
+  res.send(`<h1>Hello world</h1><p>DB connection status is <b>${DB_STATUS[mongoose.connection.readyState]}</b></p>`);
+});
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`)
 });
