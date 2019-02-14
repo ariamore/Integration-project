9b9a1759fdb0fb60143b08b1dbf0b0a6163de9dc
Mattias E. Mignone <memignone@gmail.com>
2019-02-14 14:43:17 +0100

    Don't start server if process cannot connect to DB

--- src/server.js
+++ src/server.js
@@ -10,15 +10,15 @@ MongoClient.connect(process.env.DB_URL, {useNewUrlParser: true})
 .then(connection => {
   const db = connection.db(process.env.DB_NAME)
   console.log(`The connection to ${process.env.DB_NAME} was established successfully!`)
-  app.locals.visit = db
+  app.locals.visit = db.collection('visit')
   // Monitor connection
   db.on('close', () => console.log(`CLOSE event on ${db.databaseName}`))
   db.on('error', () => console.log(`ERROR event on ${db.databaseName}`))
   db.on('reconnect', () => console.log(`RECONNECT event on ${db.databaseName}`))
   db.on('timeout', () => console.log(`TIMEOUT event on ${db.databaseName}`))})
+.then(() => {
+  // Fire request listener
+  app.listen(process.env.WEB_PORT, () =>
+    console.log(`Running on http://localhost:${process.env.WEB_PORT}`)
+  )})
 .catch(error => console.error(`An error ocurred during conection: ${error}`))
-
-// Fire request listener
-app.listen(process.env.WEB_PORT, () =>
-  console.log(`Running on http://localhost:${process.env.WEB_PORT}`)
-)
